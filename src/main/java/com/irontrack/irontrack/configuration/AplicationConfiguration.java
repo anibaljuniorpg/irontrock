c6f42a0335d4a017c370bbfe4365d5bdc9ac939c
Anibal junior <125144975+anibaljuniorpg@users.noreply.github.com>
2025-12-29 13:06:50 -0300

    Definindo a autorizacao dos endpoints
    Definindo a autorizacao dos endpoints. Programa ja completo.

--- src/main/java/com/irontrack/irontrack/configuration/AplicationConfiguration.java
+++ src/main/java/com/irontrack/irontrack/configuration/AplicationConfiguration.java
@@ -5,19 +5,45 @@ import com.irontrack.irontrack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class AplicationConfiguration {
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private TokenJwtAuthenticationFilter filter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/register" ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/workout").authenticated()
+                        .requestMatchers(HttpMethod.PUT,"/workout/{id}").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/workout/{id}").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/workout").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/workout/report").authenticated()
+
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService(){
